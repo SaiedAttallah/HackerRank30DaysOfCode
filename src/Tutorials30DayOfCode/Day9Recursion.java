c7e364d9d4d1334e8d65ff54a5b365551c6f9997
SaiedAttallah <saiedattallah@gmail.com>
2017-04-11 16:54:41 +0200

    Day 3 & 9

---
+++ src/Tutorials30DayOfCode/Day9Recursion.java
@@ -0,0 +1,26 @@
+package Tutorials30DayOfCode;
+
+import java.util.Scanner;
+
+/**
+ * Created by sattallah on 4/11/2017.
+ */
+public class Day9Recursion {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        System.out.println(factorial(n));
+    }
+
+    private static int factorial(int n){
+        // Recursive Case
+        if(n > 1) {
+            return n * factorial(n - 1);
+        }
+        // Base Case n = 1
+        else {
+            return n;
+        }
+    }
+}

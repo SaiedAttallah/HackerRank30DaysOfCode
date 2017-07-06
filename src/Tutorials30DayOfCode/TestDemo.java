612f65ee3ea30421534faf96236fe1b69a0e36ea
SaiedAttallah <saiedattallah@gmail.com>
2017-07-06 16:37:05 +0200

    Day 8

---
+++ src/Tutorials30DayOfCode/TestDemo.java
@@ -0,0 +1,23 @@
+package Tutorials30DayOfCode;
+
+/**
+ * Created by sattallah on 5/28/2017.
+ */
+public class TestDemo {
+    public static int minPieces(int[] original, int[] desired) {
+        //throw new UnsupportedOperationException("Waiting to be implemented.");
+        int i = 0, j = 0;
+        for (i = 0; i < original.length; i++){
+            if (original[i] != desired[i]){
+                j++;
+            }
+        }
+        return j;
+    }
+
+    public static void main(String[] args) {
+        int[] original = new int[] { 1, 4, 3, 2 };
+        int[] desired = new int[] { 1, 2, 4, 3 };
+        System.out.println(TestDemo.minPieces(original, desired));
+    }
+}

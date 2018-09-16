a99918c650edd2ed890356aa64fa1393eaa14392
Saied Attallah <saiedattallah@gmail.com>
2018-09-16 23:59:02 +0200

    Day10BinaryNumbers

---
+++ src/Tutorials30DayOfCode/Day10BinaryNumbers.java
@@ -0,0 +1,24 @@
+package Tutorials30DayOfCode;
+
+import java.util.Scanner;
+
+public class Day10BinaryNumbers {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        int counter = 0, max = 0;
+
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        while (n > 0) {
+            counter = ++counter * (n%2);
+            max = Math.max(counter, max);
+            n/=2;
+        }
+
+        System.out.println(max);
+
+        scanner.close();
+    }
+}
